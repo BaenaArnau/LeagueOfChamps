59af2c1c50aec214e57902d30b9653f3a59705f1
BaenaArnau <93340013+BaenaArnau@users.noreply.github.com>
2024-01-12 16:34:19 +0100

    Final

--- app/src/main/java/com/example/leagueofchamps/LoginFragment.java
+++ app/src/main/java/com/example/leagueofchamps/LoginFragment.java
@@ -3,8 +3,6 @@ package com.example.leagueofchamps;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
