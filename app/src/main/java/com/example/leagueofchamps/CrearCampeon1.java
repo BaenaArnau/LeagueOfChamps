93299cc87a9909478ec0a1eb7371d95cf5252fec
BaenaArnau <93340013+BaenaArnau@users.noreply.github.com>
2024-01-12 16:14:11 +0100

    Final

--- app/src/main/java/com/example/leagueofchamps/CrearCampeon1.java
+++ app/src/main/java/com/example/leagueofchamps/CrearCampeon1.java
@@ -12,6 +12,7 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.leagueofchamps.databinding.FragmentCrearCampeon1Binding;
 
@@ -34,18 +35,27 @@ public class CrearCampeon1 extends Fragment {
         binding.crear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nombre = binding.nombre.getText().toString();
-                String apodo = binding.descripcion.getText().toString();
-                String pasiva = binding.pasiva.getText().toString();
-                String habilidad1 = binding.habilidadEnLaQ.getText().toString();
-                String habilidad2 = binding.habilidadEnLaW.getText().toString();
-                String habilidad3 = binding.habilidadEnLaE.getText().toString();
-                String habilidad4 = binding.habilidadEnLaR.getText().toString();
+                String nombre = binding.nombre.getText().toString().trim();
+                String apodo = binding.descripcion.getText().toString().trim();
+                String pasiva = binding.pasiva.getText().toString().trim();
+                String habilidad1 = binding.habilidadEnLaQ.getText().toString().trim();
+                String habilidad2 = binding.habilidadEnLaW.getText().toString().trim();
+                String habilidad3 = binding.habilidadEnLaE.getText().toString().trim();
+                String habilidad4 = binding.habilidadEnLaR.getText().toString().trim();
 
-                elementosViewModel.insertar(new Campeon(nombre, apodo,pasiva,habilidad1,habilidad2,habilidad3,habilidad4));
-
-                navController.popBackStack();
+                if (nombre.isEmpty() || apodo.isEmpty() || pasiva.isEmpty() || habilidad1.isEmpty() ||
+                        habilidad2.isEmpty() || habilidad3.isEmpty() || habilidad4.isEmpty()) {
+                    // Mostrar un mensaje de error
+                    // Puedes utilizar un TextView en tu layout o un Toast para mostrar el mensaje de error
+                    // Aquí se usa un Toast como ejemplo:
+                    Toast.makeText(requireContext(), "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Todos los campos están completos, proceder con la inserción en la base de datos
+                    elementosViewModel.insertar(new Campeon(nombre, apodo, pasiva, habilidad1, habilidad2, habilidad3, habilidad4));
+                    navController.popBackStack();
+                }
             }
         });
+
     }
 }
